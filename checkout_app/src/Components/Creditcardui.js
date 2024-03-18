99269ec26845777eaf50abcbef7636afa93d72bd
Arijit-123 <1603074@kiit.ac.in>
2024-03-19 02:04:28 +0530

    new adjustments and putting pictures card logic

--- checkout_app/src/Components/Creditcardui.js
+++ checkout_app/src/Components/Creditcardui.js
@@ -5,17 +5,19 @@ function Creditcardui() {
   const [cvc, setCvc] = useState("");
   const [mmyy,setMmyy]=useState("");
   const [name,setName]=useState("");
-
+console.log("typeof", typeof(cardNumber));
+console.log("cardNumber",cardNumber);
   return (
     <>
     <div className='shadow-2xl w-23 h-44    bg-white-100 rounded-lg mt-4 border border-gray-300'>
+<div className='w-11/12  mx-2 mt-4 border border-gray-200 rounded-sm input flex'>
 
       <input
-      className='w-11/12  mx-2 mt-4 border border-gray-200 rounded-sm'
+      className='  w-11/12  rounded-sm input focus:outline-none'
                     type="text"
                     name="card_number"
                     id="card_number"
-                    placeholder="e.g. 1234 5678 9012 3456"
+                    placeholder="e.g. 1234 5678 9012 3456       "
                     required
                     maxLength={24}
                     value={cardNumber
@@ -24,6 +26,13 @@ function Creditcardui() {
                       .trim()}
                     onChange={(e) => setCardNumber(e.target.value)}
                   />
+
+
+{(cardNumber=="" || (cardNumber.substring(0, 1)=="4"))?<img className='h-6' src='visa.svg'></img>:""}
+{(cardNumber=="" || (cardNumber.substring(0,2)=="51") || (cardNumber.substring(0,2)=="55"))?<img className='h-6' src='mastercard_new.svg'></img>:""}
+{ (cardNumber=="" || (cardNumber.substring(0,2)=="34") || (cardNumber.substring(0,2)=="37"))?<img className='h-6' src='amex.svg'></img>:""}
+
+                  </div>
 <div className='flex mt-2'>
 <div>
 {/* <input
@@ -45,7 +54,7 @@ function Creditcardui() {
                 <div>
 
                 <input
-                  className='mt-2 ml-2 w-13 rounded-sm border border-gray-200'
+                  className='mt-2 ml-2 w-13 rounded-sm border border-gray-200 focus:outline-none'
                       type="text"
                       name=""
                       id=""
@@ -56,25 +65,26 @@ function Creditcardui() {
                       onChange={(e) => setMmyy(e.target.value)}
                     />
                 </div>
-                  <div>
+                  <div className=''>
                   <input
-                  className='mt-2 ml-2 w-14n rounded-sm border border-gray-200'
+                  className='mt-2   ml-2 w-5/6  rounded-sm border border-gray-200 focus:outline-none'
                       type="text"
                       name="cvc"
                       id="cvc"
-                      placeholder="CVV"
+                      placeholder="CVV                     "
                       maxLength={3}
                       required
                       value={cvc}
                       onChange={(e) => setCvc(e.target.value)}
                     />
+                   
                     </div>
                   </div>
                   
 </div>
 
 <input
-                  className='mt-4 ml-2 w-11/12 rounded-sm border border-gray-200'
+                  className='mt-4 ml-2 w-11/12 rounded-sm border border-gray-200 focus:outline-none'
                       type="text"
                       name=""
                       id=""
@@ -85,7 +95,7 @@ function Creditcardui() {
                       onChange={(e) => setName(e.target.value)}
                     />
                     
-<div className='border w-20 mt-4 border-blue-900 rounded-xl  items-center  text-center bg-gradient-to-r from-[#2C419A] to-[#7357A4] absolute left-36 '>
+<div className='border w-[258px] mt-4 border-blue-900 rounded-xl  items-center  text-center bg-gradient-to-r from-[#2C419A] to-[#7357A4] absolute left-14 '>
                     <button className='text-white ' >Pay</button>
                     </div>
                     
--- checkout_app/src/Components/Footerui.js
+++ checkout_app/src/Components/Footerui.js
@@ -5,19 +5,21 @@ function Footerui() {
     <div className='flex justify-center items-center h-full'>
     <div className='  md:grid grid-rows-3 grid-flow-col gap-4 sm:grid-cols-1'>
     <div className='row-end-3 mt-4 row-span-2 md:col-span-1'>
+    <img src='./icon1.png'></img>
     <label className=' text-[#5F256C] font font-bold'>Lifetime Updates</label>
-    <p className=' text-wrap mt-2 w-64'>  We are constantly extending and improving all Type Reports. Any future content we add to it you will receive for free.</p>
+    <p className=' text-wrap mt-2 w-64 text-center'>  We are constantly extending and improving all Type Reports. Any future content we add to it you will receive for free.</p>
 </div>
 
 <div className=' row-end-3 mt-4 row-span-2 md:col-span-1 '>
-<label className='text-[#5F256C] font font-bold'>30 days money back</label>
-<p className=' text-wrap mt-2 w-64'>We believe that this guide will help you so much that we will give you a refund should you feel otherwise. No questions asked!</p>
+<p  className=' text-wrap text-center'><img src='./icon2.png'></img></p>
+<p className='text-[#5F256C] font font-bold text-wrap text-center'>30 days money back</p>
+<p className=' text-wrap mt-2 w-64 text-center'>We believe that this guide will help you so much that we will give you a refund should you feel otherwise. No questions asked!</p>
 </div>
 
 <div className=' row-end-3 mt-4 row-span-2 md:col-span-1 '>
-
-<label className='text-[#5F256C] font font-bold'>Safe and Secure</label>
-<p className=' text-wrap mt-2 w-64 '>We don't record your payment details. They'll be sent over a secure, encrypted channel</p>
+<img src='./icon3.png'></img>
+<p className='text-[#5F256C] font font-bold text-wrap text-center'>Safe and Secure</p>
+<p className=' text-wrap mt-2 w-64 text-center'>We don't record your payment details. They'll be sent over a secure, encrypted channel </p>
 </div>
       
     </div>
